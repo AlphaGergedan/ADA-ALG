cbc67ef4ed963701c3518e24f1633497327f00e4
Atamert Rahma <atamertiel@gmail.com>
2021-12-02 03:25:39 +0100

    Initial file documentation

--- treaps/src/treap.hpp
+++ treaps/src/treap.hpp
@@ -1,6 +1,33 @@
+/**
+ * @file treap.hpp
+ * @brief Treap implementation.
+ *
+ * A Treap (Tree + Heap) is a binary tree where each node
+ * contains one element x with key(x) and prio(x) where
+ * the following properties hold:
+ *
+ * 1) Search Tree property: For each element x:
+ *   - elements y in the left subtree of x satisfy: key(y) < key(x)
+ *   - elements y in the right subtree of x satisfy: key(y) > key(x)
+ *   - we assume that the key values are pairwise distinct.
+ *
+ * 2) Heap property: For all elements x,y:
+ *   - if y is a child of x, then prio(y) > prio(x)
+ *   - all priorities must be pairwise distinct.
+ *
+ * For elements x_1, .. , x_n with key(x_i) and prio(x_i), there exists
+ * a unique Treap. And it has the structure that would result if
+ * elements were inserted in the order of their priorities.
+ *
+ * @author Atamert Rahma, atamertiel@gmail.com
+ */
+
 #ifndef TREAP_HPP
 #define TREAP_HPP
 
+/**
+ * Tools for Treaps
+ */
 namespace Treap {
 
   struct Pair {
