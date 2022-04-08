12e0df818f86d5f27843714a7548ef004d7aafb1
Atamert Rahma <atamertiel@gmail.com>
2022-04-08 21:00:15 +0200

    Possible new type usage + return specification

--- fast-exp/src/fast_exp.cpp
+++ fast-exp/src/fast_exp.cpp
@@ -1,14 +1,14 @@
 #include "fast_exp.hpp"
 
-int fastExp(int a, int p, int n) {
+uint64_t fastExp(uint64_t a, uint64_t p, uint64_t n) {
   if (p == 0) {
     return 1;
   }
   /* If we assume p is even, we get:
    * a^p mod n = a^(p/2) * a^(p/2) mod n
    *           = ((a^(p/2)) mod n) * ((a^(p/2)) mod n) mod n */
-  int x = fastExp(a, p/2, n);
-  int res = (x*x) % n;
+  uint64_t x = fastExp(a, p/2, n);
+  uint64_t res = (x*x) % n;
   /* Add the remaining power if p is odd */
   if (!(p % 2 == 0)) {
     res = (a * res) % n;
--- fast-exp/src/fast_exp.hpp
+++ fast-exp/src/fast_exp.hpp
@@ -1,15 +1,16 @@
 #ifndef FAST_EXP_HPP
 #define FAST_EXP_HPP
 
+#include <cstdint>
+
 /**
  * Computes a^p mod n
  *
  * @param a
  * @param p
  * @param n
- * @return
+ * @return a^p mod n
  */
-int fastExp(int a, int p, int n);
+uint64_t fastExp(uint64_t a, uint64_t p, uint64_t n);
 
 #endif
-
